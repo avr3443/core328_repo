1393bf8eba3971ef0f858c3639e1d6aa1c64dd87
arek <areklib@poczta.onet.pl>
2019-07-13 22:41:25 +0200

    Initial commit

---
+++ CoreAtmega/main.c
@@ -0,0 +1,34 @@
+/*
+ * CoreAtmega.c
+ *
+ * Created: 13.07.2019 21:20:10
+ * Author : arek1
+ */ 
+
+#include <avr/io.h>
+
+//#define F_CPU 16000000UL  // set i the project properties
+#define F_CPU 8000000UL  // set i the project properties
+#include "util/delay.h"
+
+
+// Blinking LED for tests.
+#define LCD_LED_CONFIG	DDRB |=  (1 << PB5)
+#define LCD_LED_SET		PORTB |= (1 << PB5)
+#define LCD_LED_RESET	PORTB &= ~(1 << PB5)
+
+
+int main(void)
+{
+
+	LCD_LED_CONFIG;
+
+    while (1) 
+    {
+		LCD_LED_SET;
+		_delay_ms(2000);
+		LCD_LED_RESET;
+		_delay_ms(2000);
+    }
+}
+
